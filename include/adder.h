b59b2f902f030a5ad958029c834cc7668aad57c2
wiasliaw <leewei48750@gmail.com>
2021-03-24 04:20:07 +0800

    fix(.): remove unuse file

--- include/adder.h
+++
@@ -1,4 +0,0 @@
-#ifndef ADDER_H
-#define ADDER_H
-int add(int a, int b);
-#endif
--- src/main.c
+++
@@ -1,8 +0,0 @@
-#include <assert.h>
-#include "adder.h"
-
-int main()
-{
-    assert(5 == add(2, 3));
-    return 0;
-}
